8f5d406f7ae92793c2e0213237b6828c998c6b8d
sakura_tw <81685716+yan-930521@users.noreply.github.com>
2022-01-09 06:43:05 +0000

    still working

--- testMood.js
+++ testMood.js
@@ -1,5 +1,9 @@
 (async () => {
   const ctl = require('./encapsulate.js')
+  const data = require('./Data/data.json');
+  for(let d in data.mood) {
+    console.log(`${data.mood[d].num}. ${d}`)
+  }
   const Control = new ctl();
   var Mood = await Control.createNlp('mood.nlp');
   let ans = await Control.moodExtractor(Mood, "你好阿")
